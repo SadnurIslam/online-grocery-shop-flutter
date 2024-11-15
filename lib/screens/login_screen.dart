c1dfa98e22e4852c6f4f497cf21a3e6b3f1c9cfd
Sadnur Islam <sadnurislam2000@gmail.com>
2024-11-15 18:18:22 +0600

    improve login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,10 +14,18 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.png', height: 100),
-            SizedBox(height: 50),
+            ClipOval(
+              child: Image.network(
+                'https://codeforces.com/userpic.codeforces.org/2378836/title/5c17df02f66ff8ba.jpg',
+                height: 200,
+                width: 200,
+                fit: BoxFit
+                    .cover, // Ensures the image fits within the circular frame
+              ),
+            ),
+            SizedBox(height: 20),
             Text(
-              "Welcome to Grocery App",
+              "Welcome to Grocery Shop",
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
@@ -29,6 +37,16 @@ class LoginScreen extends StatelessWidget {
                 }
               },
               child: Text("Sign in with Google"),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () async {
+                await authService.signInWithGoogle();
+                if (authService.isSignedIn) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              child: Text("Sign in with Facebook"),
             ),
           ],
         ),
